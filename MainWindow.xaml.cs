b1650151e089c205a659f2c3c653c131693fa956
Zack Morelli <zackmorelli@gmail.com>
2020-07-14 11:44:37 -0400

    Helper program for Collision Check that handles displaying the beam images using Helix Toolkit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Media3D;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using HelixToolkit.Wpf;
+
+namespace _3DRender
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow(string path)
+        {
+            InitializeComponent();
+
+            ModelVisual3D model = new ModelVisual3D();
+            model.Content = Display3d(path);
+            viewPort3d.Children.Add(model);
+        }
+
+        private Model3D Display3d(string path)
+        {
+            Model3D tmodel = null;
+            try
+            {
+                //Adding a gesture here
+                viewPort3d.RotateGesture = new MouseGesture(MouseAction.LeftClick);
+
+                //Import 3D model file
+                ModelImporter import = new ModelImporter();
+
+                //Load the 3D model file
+                tmodel= import.Load(path);
+               
+            }
+            catch (Exception e)
+            {
+                // Handle exception in case can not file 3D model
+                MessageBox.Show("Exception Error : " + e.StackTrace);
+            }
+            return tmodel;
+        }
+    }
+}
